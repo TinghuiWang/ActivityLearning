4376c8655d54eb4a03052d0104b71e3a34095ed0
Tinghui Wang <tinghui.wang@wsu.edu>
2015-12-24 08:22:19 -0800

    actlearn: model: Abstract Class Model Initial Commit
    Signed-off-by: Tinghui Wang <tinghui.wang@wsu.edu>

---
+++ actlearn/models/model.py
@@ -0,0 +1,76 @@
+import logging
+
+
+class Model(object):
+    """
+    Abstract super class that defines the interface for
+    learning models
+    """
+
+    def __init__(self, model_name):
+        """
+        Initialization Model Object
+        :param model_name: String, Model Name
+        :return:
+        """
+        self.params = []
+        self.model_name = model_name
+        self.logger = logging.getLogger(model_name)
+        pass
+
+    def cost(self, y):
+        """
+        Symbolic Tensor for cost calculation of the model
+        :param y: minibatch target class array
+        :return:
+        """
+        raise NotImplementedError()
+
+    def error(self, y):
+        """
+        Symbolic Tensor for calculating the classification
+        errors of a mini-batch input
+        :param y:
+        :return:
+        """
+        raise NotImplementedError()
+
+    def classify(self, data):
+        """
+        Symbolic Tensor for running the classifier on given
+        mini-batch input
+        :return: array of class
+        """
+        raise NotImplementedError()
+
+    # def monitor(self):
+    #     """
+    #     Return Monitor Statistics
+    #     :return:
+    #     """
+    #     raise NotImplementedError()
+
+    def save(self, filename):
+        """
+        Save current model parameters to file in Pickle format
+        :param filename: Name of the file to save the model to
+        :return:
+        """
+        raise NotImplementedError()
+
+    def load(self, filename):
+        """
+        Load saved model parameter from file
+        :param filename: Name of the file to load the model from
+        :return:
+        """
+        raise NotImplementedError()
+
+    def export(self, filename, type):
+        """
+        Export current model to file
+        :param filename: Name of the file to export the model to
+        :param type: export file type
+        :return:
+        """
+        raise NotImplementedError()
