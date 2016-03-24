6fb3e635d519f0b7216539ac0f6666e0d2ac718f
Tinghui Wang <tinghui.wang@wsu.edu>
2016-03-24 12:48:24 -0700

    actlearn: example: casas_sda: Fix Casas SDA script to use new AlResult class
    Signed-off-by: Tinghui Wang <tinghui.wang@wsu.edu>

--- examples/casas_sda/casas_sda_byweek.py
+++ examples/casas_sda/casas_sda_byweek.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import time
 import theano
 import theano.tensor as T
@@ -7,25 +6,24 @@ import cPickle as pickle
 import numpy as np
 import xlwt
 import pydot
-from actlearn.data.casas import load_casas_from_file, get_week_boundary, save_casas_learning_curve, plot_casas_learning_curve
-from actlearn.training_algorithms.cross_validation import run_cross_validation
+from actlearn.data.casas import load_casas_from_file, get_boundary
 from actlearn.models.StackedDenoisingAutoencoder import StackedDenoisingAutoencoder
 from actlearn.utils.confusion_matrix import get_confusion_matrix
-from actlearn.utils.classifier_performance import performance_index, get_performance_array
+from actlearn.utils.classifier_performance import *
 from actlearn.data.AlFeature import AlFeature
 from actlearn.utils.event_bar_plot import event_bar_plot
-
+from actlearn.utils.AlResult import AlResult
 
 if __name__ == '__main__':
     # Set current directory to local directory
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
     book = xlwt.Workbook()
     overall_sheet = book.add_sheet('overall')
-    overall_list_title = ['dataset', 'hidden units', 'correctness']
+    overall_list_title = ['dataset', 'hidden units', '#week'] + overall_performance_index
     overall_list_row = 0
     for c in range(len(overall_list_title)):
         overall_sheet.write(0, c, str(overall_list_title[c]))
-    dataset_list_title = ['activities'] + performance_index
+    dataset_list_title = ['activities'] + per_class_performance_index
     # Go through all bosch datasets
     datasets = ['b1']
     for datafile in datasets:
@@ -47,50 +45,76 @@ if __name__ == '__main__':
         test_index = []
         x_tensor = theano.shared(np.asarray(feature.x, dtype=theano.config.floatX), borrow=True)
         y_tensor = T.cast(theano.shared(feature.y, borrow=True), 'int32')
-        week_array = get_week_boundary(feature)
+        week_array = get_boundary(feature, period='week')
         # Number of perceptrons in hidden layer
         hidden_layer_list = [[200, 200, 200]]
         for hidden_layer in hidden_layer_list:
             input_x = T.matrix('x')
             num_classes = feature.num_enabled_activities
             numpy_rng = np.random.RandomState(int(time.clock()))
+            # Set Training Rate and Corruption Levels
+            pre_training_learning_rate = []
+            for i in range(2*len(hidden_layer)):
+                pre_training_learning_rate.append(0.5)
+            fine_tuning_training_rate = []
+            for i in range(2*len(hidden_layer) + 2):
+                fine_tuning_training_rate.append(0.1)
+            corruption_levels = []
+            for i in range(len(hidden_layer)):
+                corruption_levels.append(0)
+            pre_training_epochs = int(len(hidden_layer) * hidden_layer[0] / 30)
+            fine_tuning_epochs = int(len(hidden_layer) * hidden_layer[0] / 10)
+            # Create Model
             model = StackedDenoisingAutoencoder(numpy_rng=numpy_rng, input=input_x,
                                                 n_ins=feature.x.shape[1], n_outs=num_classes,
                                                 hidden_layers_sizes=hidden_layer,
-                                                corruption_levels=[0, 0, 0])
-            learning_result = []
+                                                corruption_levels=corruption_levels)
+            learning_result_fname = 'sda_' + datafile + '_' + \
+                '_'.join([('%d' % hidden_layer[layer]) for layer in range(len(hidden_layer))]) + '.pkl'
+            result_name = '%s sda %d layers %d nodes' % (datafile, len(hidden_layer), hidden_layer[0])
+            learning_result = AlResult(result_name=result_name, data_fname=datafile, mode='by_week')
+            if os.path.exists(learning_result_fname):
+                learning_result.load_result(learning_result_fname)
             for week_id in range(len(week_array) - 1):
                 if week_id == 0:
                     train_index = range(0, week_array[week_id])
                 else:
                     train_index = range(week_array[week_id - 1], week_array[week_id])
                 test_index = range(week_array[week_id], week_array[week_id + 1])
-                # Load MLP if exists
-                sda_filename = datafile + '_' + \
-                    '_'.join([('%d' % hidden_layer[layer]) for layer in range(len(hidden_layer))]) + \
-                    ('_week%03d.pkl' % week_id)
-                if os.path.exists(sda_filename):
-                    # Load Model from File
-                    model.load(sda_filename)
-                else:
+                # Performance Evaluation
+                result_key = 'week %d' % week_id
+                result = learning_result.get_result_by_key(result_key)
+                if result is None:
                     model.do_pretraining(data=x_tensor[train_index],
                                          num_data=len(train_index), batch_size=10,
-                                         learning_rate_array=[0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
-                                         num_epochs=15)
+                                         learning_rate_array=pre_training_learning_rate,
+                                         num_epochs=pre_training_epochs)
                     model.do_fine_tuning(data=x_tensor[train_index], label=y_tensor[train_index],
                                          num_data=len(train_index), batch_size=10,
-                                         learning_rate_array=[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
-                                         num_epochs=10)
-                    model.save(sda_filename)
-                # Performance Evaluation
-                result = model.classify(x_tensor[test_index])
-                predicted_y = result[0]
-                confusion_matrix = get_confusion_matrix(num_classes=feature.num_enabled_activities,
-                                                        label=feature.y[test_index], predicted=predicted_y)
-                correctness = (confusion_matrix.trace() / float(predicted_y.shape[0])) * 100
-                learning_result.append(correctness)
-                training_stat = get_performance_array(num_classes=feature.num_enabled_activities,
-                                                      confusion_matrix=confusion_matrix)
+                                         learning_rate_array=fine_tuning_training_rate,
+                                         num_epochs=fine_tuning_epochs)
+                    (predicted_y,) = model.classify(x_tensor[test_index])
+                    confusion_matrix = get_confusion_matrix(num_classes=feature.num_enabled_activities,
+                                                            label=feature.y[test_index], predicted=predicted_y)
+                    (overall_performance, per_class_performance) = \
+                        get_performance_array(num_classes=feature.num_enabled_activities,
+                                              confusion_matrix=confusion_matrix)
+                    learning_result.add_result(model.export_to_dict(), key=result_key,
+                                               confusion_matrix=confusion_matrix,
+                                               overall_performance=overall_performance,
+                                               per_class_performance=per_class_performance)
+                else:
+                    model.load_from_dict(result['model'])
+                    confusion_matrix = result['confusion_matrix']
+                    overall_performance = result['overall_performance']
+                    per_class_performance = result['per_class_performance']
+                # casas_where_are_the_errors(feature.y[test_index], predicted_y,
+                #                            label_array=[feature.get_activity_by_index(k)
+                #                                         for k in range(feature.num_enabled_activities)],
+                #                            filename=(datafile + '_' +
+                #                                '_'.join([('%d' % hidden_layer[layer])
+                #                                          for layer in range(len(hidden_layer))]) +
+                #                                ('_week%03d' % week_id) + '_error_summary.pkl'))
                 # Draw out the event graph
                 # event_bar_plot(feature.time[test_index], feature.y[test_index], feature.num_enabled_activities,
                 #                classified=predicted_y, max_days=10,
@@ -99,8 +123,9 @@ if __name__ == '__main__':
                 overall_list_row += 1
                 overall_sheet.write(overall_list_row, 0, datafile)
                 overall_sheet.write(overall_list_row, 1, hidden_layer.__str__())
-                overall_sheet.write(overall_list_row, 2, '%.5f' % correctness)
-                overall_sheet.write(overall_list_row, 3, ('week %3d' % week_id))
+                overall_sheet.write(overall_list_row, 2, ('week %3d' % week_id))
+                for c in range(len(overall_performance_index)):
+                    overall_sheet.write(overall_list_row, c + 3, '%.5f' % overall_performance[c])
                 dataset_sheet = book.add_sheet(datafile + '_' +
                                                '_'.join([('%d' % hidden_layer[layer])
                                                          for layer in range(len(hidden_layer))]) +
@@ -108,17 +133,11 @@ if __name__ == '__main__':
                                                )
                 for c in range(0, len(dataset_list_title)):
                     dataset_sheet.write(0, c, str(dataset_list_title[c]))
-                num_performance = len(performance_index)
+                num_performance = len(per_class_performance_index)
                 for r in range(feature.num_enabled_activities):
                     activity_label = feature.get_activity_by_index(r)
                     dataset_sheet.write(r+1, 0, activity_label)
                     for c in range(num_performance):
-                        dataset_sheet.write(r+1, c+1, '%.5f' % training_stat[r][c])
+                        dataset_sheet.write(r+1, c+1, per_class_performance[r][c])
                 book.save('casas_sda_byweek.xls')
-            learning_curve_fname = 'sda_learning_' + datafile + '_' + \
-                                   '_'.join([('%d' % hidden_layer[layer]) for layer in range(len(hidden_layer))]) + \
-                                   '.pkl'
-            save_casas_learning_curve(learning_curve_fname,
-                                      '%s sda %d of %d nodes' % (datafile, len(hidden_layer), hidden_layer[0]),
-                                      learning_result, 'week')
-    plot_casas_learning_curve(['sda_learning_b1_200_200_200.pkl'])
+                learning_result.save_result(learning_result_fname)
